997a47da08129f1091cb535847a5b4dd46aa38fe
SHermukhammad <shermukhammadtemirov@gmail.com>
2023-04-22 12:09:23 +0500

    I have finished index3 file

---
+++ section1/index2.dart
@@ -0,0 +1,17 @@
+// VARIABLES and DATA TYPES
+
+// Number 
+// int - butun sonlar
+// double  - qoldiq sonlar
+// Boolean - True va False
+// List -  ro'yxat
+// Maps  -  xaritalar
+// Symbols - simvollar
+
+
+
+void main(List<String> args) 
+{
+  int integer = 10;
+  print(integer);
+}
---
+++ section1/index3.dart
@@ -0,0 +1,38 @@
+void main(List<String> args) {
+  int age = 30;
+  // print(age);
+  // print('age');
+
+  age = 31;
+  age = 33.4.toInt();
+  print(age);
+
+  double massa = 100.7;
+  print(massa);
+
+  massa = 101;
+  print(massa);
+
+  num number = 83;
+  number = 88.25;
+  print(number);
+
+  int hexadecimalNumber = 0xAABBCC;
+  print(hexadecimalNumber);
+
+  double exponentNum = 1.42e5;
+  print(exponentNum);
+
+  var son2 = 50;
+  print(son2);
+  son2 = 60;
+
+  bool isadult = true;
+  print(isadult);
+
+  var isblack = false;
+  print(isblack);
+
+
+
+}
