4a42d94302b073409339a0b8daa13e200fb9e68b
SHermukhammad <shermukhammadtemirov@gmail.com>
2023-04-24 10:40:36 +0500

    I have finished index5

---
+++ section1/index5.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  final String city = 'Tashkent';
+  // city = "samarkand"; X
+  print('City: $city');
+
+  const PI = 3.14;
+  // PI = 4.2; X
+  print("PI: $PI");
+  
+}
