0414d9de21ff1ea05aa90c87ea38cad8e1f580cd
SHermukhammad <shermukhammadtemirov@gmail.com>
2023-11-13 16:18:19 +0500

    Update main.dart

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -24,68 +24,19 @@ class MyHome extends StatefulWidget {
 class _MyHome extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(flex: 2 ,child: Container(color: Colors.amber)),
-          const Divider(color: Colors.black),
-          Expanded(
-            flex: 3,
-              child: Container(
-                  margin: EdgeInsets.all(20.0),
-                  child: Column(
-                    children: <Widget>[
-                      Expanded(
-                          child: Row(
-                           mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                          ElevatedButton(
-
-                            style: ElevatedButton.styleFrom(
-
-                                backgroundColor: Colors.white70,
-                                shape: const CircleBorder(),
-                                padding: const EdgeInsets.all(30)),
-
-                            child: Text("1", style: TextStyle(fontSize: 20)),
-                            onPressed: () {},
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                shape: const CircleBorder(),
-                                padding: const EdgeInsets.all(30)),
-                            child: const Icon(
-                              Icons.add,
-                              size: 15,
-                            ),
-                            onPressed: () {},
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                shape: const CircleBorder(),
-                                padding: const EdgeInsets.all(30)),
-                            child: const Icon(
-                              Icons.add,
-                              size: 15,
-                            ),
-                            onPressed: () {},
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                shape: const CircleBorder(),
-                                padding: const EdgeInsets.all(30)),
-                            child: const Icon(
-                              Icons.add,
-                              size: 15,
-                            ),
-                            onPressed: () {},
-                          ),
-                        ],
-                      )),
-                    ],
-                  )))
-        ],
-      ),
+    return SafeArea(child:
+      Scaffold(
+        body: Column(
+            children: <Widget>[
+              Expanded(child:
+              Container(
+                margin: EdgeInsets.all(20.0),
+                alignment: Alignment.topRight,
+                  child: Text("0"*20, style: TextStyle(fontSize: 48))
+              ))
+            ]
+        )
+    )
     );
   }
 }
